2e40da738952ddbafdde01f5dca81728a2778404
Seyed Iman Modarres Sadeghi <smodarres-sadeghi@myseneca.ca>
2023-03-02 19:56:29 -0500

    added a comment in 01

--- 01-starting-setup/src/index.js
+++ 01-starting-setup/src/index.js
@@ -1,7 +1,8 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//Rendering App component
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
