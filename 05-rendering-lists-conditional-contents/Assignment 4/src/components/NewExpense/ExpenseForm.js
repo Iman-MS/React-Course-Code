eb7a7e8012df7c7c6abf307f01e6f28be330241f
Seyed Iman Modarres Sadeghi <smodarres-sadeghi@myseneca.ca>
2023-03-03 15:29:03 -0500

    edited assignment 4

--- 05-rendering-lists-conditional-contents/Assignment 4/src/components/NewExpense/ExpenseForm.js
+++ 05-rendering-lists-conditional-contents/Assignment 4/src/components/NewExpense/ExpenseForm.js
@@ -108,7 +108,9 @@ const ExpenseForm = (props) => {
           </div>
         </div>
         <div className="new-expense__actions">
-          <button onClick={cancelHandler}>Cancel</button>
+          <button onClick={cancelHandler} type="button">
+            Cancel
+          </button>
           <button type="submit">Add Expense</button>
         </div>
       </form>
